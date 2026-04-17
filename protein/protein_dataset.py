f8e9a8ef206e522d2e7859962538d267efd24d22
elrbe <elrbergeron@gmail.com>
2026-04-17 13:52:53 +0200

    feat: optimize file reading

--- protein/protein_dataset.py
+++ protein/protein_dataset.py
@@ -64,10 +64,12 @@ class EmbeddingProteinQueryDatasetNoIO(Dataset):
 
 class EmbeddingProteinQueryDatasetIO(Dataset):
     """
-    Dataset reading the original file at each retrieval
+    Dataset reading the original file efficiently from disk without RAM overload.
     """
     def __init__(self, file_path):
         self.file_path = file_path
+        self.file = None 
+        
         with h5py.File(self.file_path, "r") as f:
             self.length = f["embeddings"].shape[0]
 
@@ -75,11 +77,14 @@ class EmbeddingProteinQueryDatasetIO(Dataset):
         return self.length
 
     def __getitem__(self, idx):
-        with h5py.File(self.file_path, "r") as f:
-            embedding = torch.from_numpy(f["embeddings"][idx])
-            cell_id = f["cell_ids"][idx]
+        # Open once per worker
+        if self.file is None:
+            self.file = h5py.File(self.file_path, "r")
             
-            if isinstance(cell_id, bytes):
-                cell_id = cell_id.decode("utf-8")
+        embedding = torch.from_numpy(self.file["embeddings"][idx])
+        cell_id = self.file["cell_ids"][idx]
+        
+        if isinstance(cell_id, bytes):
+            cell_id = cell_id.decode("utf-8")
                 
         return embedding, cell_id
